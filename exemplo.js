db3aeb041d0a6c8f43bedd6e2bad74f26d6f79e6
Your Name <you@example.com>
2018-01-23 22:46:33 -0200

    ajustes em aula

---
+++ exemplo.js
@@ -0,0 +1,15 @@
+let botao = document.querySelector('button')
+let subtitulo = document.querySelector('h2')
+let foto1 = document.querySelector('imagem10')
+
+
+function CorTexto() {
+  subtitulo.style.color = 'red';
+}
+
+function MudaImagem(){
+  foto1.
+  }
+
+
+}
